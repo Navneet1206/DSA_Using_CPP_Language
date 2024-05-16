17e9880c07b89f26fbab8e6de8cd6402092e05a2
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-05-16 14:34:20 +0530

    --

---
+++ Time_and_Space_complexity_practice/sum_between_two_number.cpp
@@ -0,0 +1,38 @@
+//Experimental Analysis
+
+
+#include <iostream>
+using namespace std;
+int alogrithm_using_loop(int num1, int num2)
+{
+    int result = 0;
+    for (int i = num1; i <= num2; i++)  
+    {
+        result += i;
+    }
+    return result;
+}
+int algorithm_using_formula(int num1, int num2)
+{
+    int n = (num2 - num1 + 1);
+    int a = num1;
+
+    int result = (n / 2)*(2 * a + (n - 1) * 1);
+    return result;
+}
+int main()
+{
+    int x, y, answer;
+    cout << "Enter the x : ";
+    cin >> x;
+    cout << "Enter the y greater than x : ";
+    cin >> y;
+
+    answer = alogrithm_using_loop(x, y);
+    cout << "\nResult alogrithm_using_loop : " << answer;
+
+    answer = 0;
+    answer = algorithm_using_formula(x, y);
+    cout << "\nResult algorithm_using_formula :  " << answer;
+    return 0;
+}
