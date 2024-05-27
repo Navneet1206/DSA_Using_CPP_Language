be8970845761ff1375683caf78b2db484b6c7665
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-05-27 21:38:30 +0530

    insertion at random position is added

--- Linked-List/singly-linked-list/insertion.cpp
+++ Linked-List/singly-linked-list/insertion.cpp
@@ -1,55 +1,84 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 struct node
 {
     int data;
-    node* next;
+    node *next;
 };
-void insertion_at_beginning(node* &head, int newdata)
+void insertion_at_beginning(node *&head, int newdata)
 {
-    node*  newnode =new node();
+    node *newnode = new node();
     newnode->data = newdata;
-    newnode->next=head;
-    head =newnode;
+    newnode->next = head;
+    head = newnode;
 }
-void insertion_at_end(node* &head, int newdata)
+void insertion_at_end(node *&head, int newdata)
 {
-    node * newnode = new node();
-   newnode -> data = newdata;
-   newnode -> next =nullptr;
-   
-   if(head == nullptr)
-   {
+    node *newnode = new node();
+    newnode->data = newdata;
+    newnode->next = nullptr;
+
+    if (head == nullptr)
+    {
         head = newnode;
         return;
-   }
-   
-   node* last = head;
-   while(last->next != nullptr)
-   {
+    }
+
+    node *last = head;
+    while (last->next != nullptr)
+    {
         last = last->next;
-   }
+    }
     last->next = newnode;
 }
-void display(node* temp)
+
+void insertion_at_random_position(node *&head, int newdata, int position)
 {
-    while(temp!=nullptr)
+    node *newnode = new node();
+    newnode->data = newdata;
+    newnode->next = nullptr;
+    if (head == nullptr)
     {
-        cout<<temp->data<<" -> ";
-        temp=temp->next;
+
+        head = newnode;
+        return;
+    }
+    if (position == 0)
+    { // If inserting at the beginning
+        newnode->next = head;
+        head = newnode;
+        return;
+    }
+
+    node *temp = head;
+    for (int i = 0; i < position - 1 && temp->next != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
+void display(node *temp)
+{
+    while (temp != nullptr)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
     }
     cout << "Null" << endl;
 }
 int main()
 {
-    node* head = nullptr;
-    insertion_at_beginning(head,0);
-    insertion_at_beginning(head,10);
-    insertion_at_end(head,20);
-    insertion_at_end(head,30);
-    insertion_at_end(head,40);
-    insertion_at_end(head,50);
-    cout<<"Linked list \n ";
+    node *head = nullptr;
+    insertion_at_beginning(head, 0);
+    insertion_at_beginning(head, 10);
+    insertion_at_random_position(head, 80, 3);
+    insertion_at_end(head, 20);
+    insertion_at_end(head, 30);
+    insertion_at_end(head, 40);
+    insertion_at_end(head, 50);
+    cout << "Linked list \n ";
     display(head);
-    return  0;
+    return 0;
 }
