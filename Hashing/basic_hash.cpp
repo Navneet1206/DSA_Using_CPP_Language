2cfa1073bf674981c3be0e82eec25ecc735d9115
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-07-26 22:09:30 +0530

    Basic hasing function documentation is added

--- Hashing/basic_hash.cpp
+++ Hashing/basic_hash.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    int arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int n, fun = 0;
+    int arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Initiallization of the array with zero value
+    int n, fun = 0;                               // fun is used to get functional value
     cout << "enter the values from 1-10 : ";
     cin >> n;
     cout << "enter the numbers : ";
@@ -11,26 +11,27 @@ int main()
     {
         int x = NULL;
         cin >> x;
-        fun = x % 10;
-        arr[fun] = x;
+        fun = x % 10; // hasing function is used where it take the element as the mod of 10
+        arr[fun] = x; // here the mod of 10 value is consider as the index of the keys
     }
-    cout << endl<<"Inserted elements\n";
-    for(int i = 0; i < 10; i++)
+    cout << endl
+         << "Inserted elements\n"; // printing the inserted elements how these are stored
+    for (int i = 0; i < 10; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
     int target;
     cout << "enter the target number : ";
-    cin >> target;
+    cin >> target; // Taking the target number from the user
 
-    fun = 0;
-    fun = target % 10;
-    if (arr[fun] == target)
+    fun = 0;                // intiallizing the function with zero for prevent irregularity
+    fun = target % 10;      // Again hash function operation is used
+    if (arr[fun] == target) // Checking the condition if target is equal to the index solved by hash function
     {
         cout << "Element is present in the index of : " << fun << endl;
     }
-    else
+    else // if not them it not inserted
     {
         cout << "Element is not inserted";
     }
