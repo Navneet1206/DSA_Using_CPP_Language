87fdd6d295466965cbe9c4a86cfd513e8365efb4
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-07-29 17:53:14 +0530

    Basic hasmap operations are added

---
+++ Hashing_and_hashmap/basic_hashmap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+    map<string, int> directory;
+
+    directory["Hello"] = 121321;
+    directory["World"] = 21312;
+    directory["Raj"] = 42232;
+
+    for (auto element : directory)
+    {
+        cout << "Name - " << element.first << endl;
+        cout << "Number - " << element.second << endl;
+    }
+
+    return 0;
+}
