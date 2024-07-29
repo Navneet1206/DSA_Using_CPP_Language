d6c1ed2eb2c0fa6af6305306e68f074e42812123
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-07-29 20:37:54 +0530

    update 02

--- Hashing_and_hashmap/basic_hashmap.cpp
+++ Hashing_and_hashmap/basic_hashmap.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 int main()
 {
-    map<string, int> directory;
+    map<string, int> directory = {{"harry", 8435}, {"coding", 1213}}; // we can initiallize like this also
     // map<string, int, greater<string>> directory;
 
-    directory["Hello"] = 121321;
+    directory["Hello"] = 121321; // this type also used to intialize
     directory["World"] = 21312;
     directory["Raj"] = 42232;
+    directory.insert(make_pair("Bob", 2322)); // this type also used to insert value
 
-    for (auto element : directory)
+    for (auto element : directory) // this is for each loop used to traverse and element is just a variable
     {
-        cout << "Name - " << element.first << endl;
-        cout << "Number - " << element.second << endl;
+        cout << "Name - " << element.first << endl;    // .first point to the key
+        cout << "Number - " << element.second << endl; // .second  sub value of the key
     }
 
     return 0;
