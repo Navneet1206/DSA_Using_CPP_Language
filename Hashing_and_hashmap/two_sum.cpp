93f4e70136663569e3b9bd03574c12bc231d902e
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-08-11 17:14:02 +0530

    Two Sum Program is added

---
+++ Hashing_and_hashmap/two_sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (map.find(complement) != map.end()) {
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    return 0;
+}
